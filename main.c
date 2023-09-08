5a22ee5960dd6e4dea527e8dbea95dbfc2cfd6e1
Hai Pham <ngochai285nd@gmail.com>
2023-09-09 01:29:24 +0700

    update

--- main.c
+++ main.c
@@ -1,26 +1,41 @@
-/*
-    Warp-Cloudflare-GUI
-*/
-
-#include <gtk/gtk.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <stdbool.h>
+#include <gtk/gtk.h>
 #include <pthread.h>
 
 GtkWidget *window;
 GtkWidget *switch_button;
+pthread_mutex_t switch_mutex = PTHREAD_MUTEX_INITIALIZER;
+bool switch_active = false;
 
-void switch_button_callback(GtkWidget *widget, GParamSpec *pspec, gpointer data)
+bool check_connection_status()
 {
-    if (gtk_switch_get_active(GTK_SWITCH(switch_button)))
+    int result = system("warp-cli status | grep \"Status update: Connected\" > /dev/null 2>&1");
+    if (WIFEXITED(result) && WEXITSTATUS(result) == 0)
     {
-        system("warp-cli connect > /dev/null");
+        return true;
     }
     else
     {
-        system("warp-cli disconnect > /dev/null");
+        return false;
     }
+}
+
+void set_switch_state(bool active)
+{
+    pthread_mutex_lock(&switch_mutex);
+    if (switch_active != active)
+    {
+        switch_active = active;
+        gtk_switch_set_active(GTK_SWITCH(switch_button), switch_active);
+    }
+    pthread_mutex_unlock(&switch_mutex);
+}
+
+void quit_program(GtkWidget *widget, gpointer data)
+{
+    gtk_main_quit();
 }
 
 int main(int argc, char *argv[])
@@ -34,23 +49,24 @@ int main(int argc, char *argv[])
     // Load the UI file
     gtk_builder_add_from_file(builder, "ui.glade", NULL);
 
+    // Connect signal handlers to the constructed widgets.
+    gtk_builder_connect_signals(builder, NULL);
+
     // Get the window object
     window = GTK_WIDGET(gtk_builder_get_object(builder, "main_window"));
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    g_signal_connect(window, "destroy", G_CALLBACK(quit_program), NULL);
 
     // Get the switch button object
     switch_button = GTK_WIDGET(gtk_builder_get_object(builder, "switch_button"));
-    if (system("warp-cli status | grep \"Status update: Connected\" > /dev/null") == 0)
-    {
-        gtk_switch_set_active(GTK_SWITCH(switch_button), TRUE);
-    }
-    else
-    {
-        gtk_switch_set_active(GTK_SWITCH(switch_button), FALSE);
-    }
+    set_switch_state(check_connection_status());
 
-    // Connect the switch button callback
-    g_signal_connect(switch_button, "notify::active", G_CALLBACK(switch_button_callback), NULL);
+    // Load the CSS file
+    GtkCssProvider *css_provider = gtk_css_provider_new();
+    GdkDisplay *display = gdk_display_get_default();
+    GdkScreen *screen = gdk_display_get_default_screen(display);
+    gtk_style_context_add_provider_for_screen(screen, GTK_STYLE_PROVIDER(css_provider), GTK_STYLE_PROVIDER_PRIORITY_USER);
+    gtk_css_provider_load_from_path(css_provider, "styles/style.css", NULL);
+    g_object_unref(css_provider);
 
     // Destroy the builder
     g_object_unref(builder);
